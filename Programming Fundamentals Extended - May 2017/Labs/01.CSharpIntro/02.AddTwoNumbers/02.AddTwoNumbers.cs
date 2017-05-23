e437669bc58e051f63094b1995d563b08f275f33
radoslavvv <radoslav.varadinov@gmail.com>
2017-05-23 23:38:21 +0300

    Added new course folder

---
+++ Programming Fundamentals Extended - May 2017/Labs/01.CSharpIntro/02.AddTwoNumbers/02.AddTwoNumbers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class AddTwoNumbers
+{
+    static void Main()
+    {
+        int firstNumber = int.Parse(Console.ReadLine());
+        int secondNumber = int.Parse(Console.ReadLine());
+        int sum = firstNumber + secondNumber;
+
+        Console.WriteLine($"{firstNumber} + {secondNumber} = {sum}");
+    }
+}
+
---
+++ Programming Fundamentals Extended - May 2017/Labs/01.CSharpIntro/03.EmployeeData/03.EmployeeData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main()
+    {
+        string name = Console.ReadLine();
+        int age = int.Parse(Console.ReadLine());
+        string id = Console.ReadLine();
+
+        id = id.PadLeft(8, '0');
+        decimal salary = decimal.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Name: {name}");
+        Console.WriteLine($"Age: {age}");
+        Console.WriteLine($"Employee ID: {id:d8}");
+        Console.WriteLine($"Salary: {salary:0.00}");
+    }
+}
+
+//MyString.PadLeft(20, '-')
